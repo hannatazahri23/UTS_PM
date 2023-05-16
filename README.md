ef3eeecb99d9040e5dcfd1ca913db06e6e2e82e2
hannatazahri23 <hannatadjava23@gmail.com>
2023-05-16 12:35:16 +0700

    Done 99%

---
+++ README.md
@@ -0,0 +1,19 @@
+| # | Biodata |
+| -------- | --- |
+| **Nama** | M. Hannata Zahri |
+| **NIM** | 312010318 |
+| **Kelas** | TI.20.A.2 |
+| **Mata Kuliah** | Pemrograman Mobile |
+
+# Ujian Tengah Semester - Semester 6
+
+## Membuat halaman login dengan Flutter
+
+* tampilan login page
+![LoginPage](img/LoginPage.png)
+
+* tampilan login page ketika salah memasukan username 
+![LoginWrong](img/LoginWrong.png)
+
+* tampilan home page
+![HomePage](img/HomePage.png)
---
+++ belajar_flutter/lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(LoginApp());
+}
+
+class LoginApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _loginButtonPressed(BuildContext context) {
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+
+    // Lakukan validasi login
+    // ...
+
+    // Contoh validasi sederhana
+    if (username == 'admin' && password == 'admin123') {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Login Gagal'),
+          content: Text('Username atau password salah.'),
+          actions: [
+            TextButton(
+              child: Text('OK'),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'logo.png', // Ubah dengan path gambar login Anda
+              height: 150,
+            ),
+            SizedBox(height: 24.0),
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: 'Username',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 24.0),
+            ElevatedButton(
+              onPressed: () => _loginButtonPressed(context),
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Selamat datang di halaman utama!'),
+      ),
+    );
+  }
+}
